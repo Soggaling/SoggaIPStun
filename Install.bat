47671339dc912d9d09861f9ab13b2e8996ba71ac
Soggaling <126654797+Soggaling@users.noreply.github.com>
2026-03-13 23:09:37 +1300

    Update Install.bat

--- Install.bat
+++ Install.bat
@@ -5,7 +5,9 @@ title SoggaIPPuller Download
 echo [+] Downloading SoggaIPPuller...
 powershell -NoProfile -Command "Invoke-WebRequest -Uri 'https://www.dropbox.com/scl/fi/r0d1x2s361si92lk19u71/SoggaIPPuller.cmd?rlkey=uh2yruc2h4kvld5xxqpdlu1by&st=wz4tumtt&dl=1' -OutFile '%userprofile%\downloads\SoggaIPPuller.cmd'"
 
-if exist "%userprofile%\Desktop\SoggaIPPuller.cmd" (
+timeout /t 2 /nobreak
+
+if exist "%userprofile%\downloads\SoggaIPPuller.cmd" (
     echo [+] Download Complete! Running file...
     start "" "%userprofile%\downloads\SoggaIPPuller.cmd"
 ) else (
